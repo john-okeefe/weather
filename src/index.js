7342f18f1bd210ed1090ff8ee05e45eed304abd4
John O'Keefe <jokeefe@fastmail.com>
2017-02-12 21:30:19 -0600

    saved API Key

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { createStore, applyMiddleware } from 'redux';
 import App from './components/app';
 import reducers from './reducers';
 
+const API_KEY = '3097d9e29cdafbb58d604d1f9518184e'
+
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
