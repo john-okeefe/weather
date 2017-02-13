94bb99b998adb4e4f3cfd3dbd5c7cd4a941b1a69
John O'Keefe <jokeefe@fastmail.com>
2017-02-12 23:24:35 -0600

    wanted an upload

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
 const API_KEY = '3097d9e29cdafbb58d604d1f9518184e';
+//used es6 template string. Nice!
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?APPID=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = fetch(url);
+
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
