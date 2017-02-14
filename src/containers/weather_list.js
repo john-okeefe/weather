aac5f56bf3b7775c13c86a25984c05b9b6e43150
John O'Keefe <jokeefe@fastmail.com>
2017-02-14 15:18:12 -0600

    on video adding sparklines at 5:19

---
+++ src/containers/weather_list.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Sparklines } from 'react-sparklines';
+
+class WeatherList extends Component {
+	renderWeather(cityData){
+		const name = cityData.city.name;
+		const temps = cityData.list.map(weather => weather.main.temp)
+
+		return (
+			<tr key={name}>
+				<td>{name}</td>
+				<td><Sparklines data={temps} width={100} height={20}><SparklinesLine color="blue" /></Sparklines></td>
+			</tr>
+			)
+	}
+
+	render() {
+		return (
+			<table className="table table-hover">
+				<thead>
+					<tr>
+						<th>City</th>
+						<th>Temperature</th>
+						<th>Pressure</th>
+						<th>Humidity</th>
+					</tr>
+				</thead>
+				<tbody>
+					{this.props.weather.map(this.renderWeather)}
+				</tbody>
+			</table>
+		);
+	}
+}
+
+function mapStateToProps({ weather }){
+	return { weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
